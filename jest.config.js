6802e6e58077ee3caa6765aa0d8401cf1227fda0
Denn4ik2010 <yakimivdenis75@gmail.com>
2024-08-19 11:30:18 +0300

    Added docker compose file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+    testEnvironment: 'node',
+    transform: {
+        '^.+.tsx?$': ['ts-jest', {}],
+    },
+    verbose: true,
+    silent: false,
 };
